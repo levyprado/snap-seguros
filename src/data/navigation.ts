3d719fe86c62fd2886b4e0c28adf9fa0367f8852
Levy Prado <levyprado28@gmail.com>
2026-03-06 00:42:01 -0400

    feat: add navigation items data

---
+++ src/data/navigation.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { label: 'Operadoras', id: '#operadoras' },
+  { label: 'Planos', id: '#planos' },
+  { label: 'Benefícios', id: '#beneficios' },
+  { label: 'Depoimentos', id: '#depoimentos' },
+  { label: 'FAQ', id: '#faq' },
+  { label: 'Contato', id: '#contato' },
+]
