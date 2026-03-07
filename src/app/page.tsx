eb5a7c2458333ea49d2950d1a44bbd963cb42c3a
Levy Prado <levyprado28@gmail.com>
2026-03-06 23:25:47 -0400

    feat: integrate TrustBar section into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
 import Hero from '@/components/sections/hero'
+import TrustBar from '@/components/sections/trust-bar'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <TrustBar />
       </main>
       <Footer />
     </>
