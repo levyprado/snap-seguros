8f217a06011154caac429ea5f8e8d932c229cfdd
Levy Prado <levyprado28@gmail.com>
2026-03-07 20:23:43 -0400

    feat: create insurance providers data

---
+++ src/data/operadoras.ts
@@ -0,0 +1,19 @@
+import amilImg from '@/assets/images/operadoras/amil.png'
+import assimImg from '@/assets/images/operadoras/assim.png'
+import bradescoImg from '@/assets/images/operadoras/bradesco.png'
+import leveImg from '@/assets/images/operadoras/leve.png'
+import notredameImg from '@/assets/images/operadoras/notredame.png'
+import portoImg from '@/assets/images/operadoras/porto.png'
+import sulamericaImg from '@/assets/images/operadoras/sulamerica.png'
+import unimedImg from '@/assets/images/operadoras/unimed.png'
+
+export const operadoras = [
+  { name: 'Amil', logoUrl: amilImg },
+  { name: 'Bradesco Saúde', logoUrl: bradescoImg },
+  { name: 'SulAmérica', logoUrl: sulamericaImg },
+  { name: 'Assim Saúde', logoUrl: assimImg },
+  { name: 'Unimed', logoUrl: unimedImg },
+  { name: 'Porto', logoUrl: portoImg },
+  { name: 'NotreDame Intermédica', logoUrl: notredameImg },
+  { name: 'Leve Saúde', logoUrl: leveImg },
+]
