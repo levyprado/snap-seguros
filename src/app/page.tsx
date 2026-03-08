8d69a28d886aa6212099f0bae98b44338e74b6d7
Levy Prado <levyprado28@gmail.com>
2026-03-07 20:25:46 -0400

    feat: integrate operadoras section into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
 import Hero from '@/components/sections/hero'
+import Operadoras from '@/components/sections/operadoras'
 import TrustBar from '@/components/sections/trust-bar'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <main>
         <Hero />
         <TrustBar />
+        <Operadoras />
       </main>
       <Footer />
     </>
