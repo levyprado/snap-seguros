04beae77f98f43dddc744ce3d4b94724b68b624d
Levy Prado <levyprado28@gmail.com>
2026-03-07 22:56:31 -0400

    feat: create steps data

---
+++ src/data/steps.ts
@@ -0,0 +1,33 @@
+import {
+  ChartIcon,
+  ChatIcon,
+  CheckmarkCircleIcon,
+  ThumbsUpIcon,
+} from '@hugeicons/core-free-icons'
+
+export const steps = [
+  {
+    icon: ChatIcon,
+    title: 'Fale Conosco',
+    description:
+      'Entre em contato por WhatsApp, telefone ou formulário. Leva menos de 2 minutos.',
+  },
+  {
+    icon: ChartIcon,
+    title: 'Receba a Análise',
+    description:
+      'Comparamos planos de todas as operadoras para encontrar a melhor relação custo-benefício.',
+  },
+  {
+    icon: CheckmarkCircleIcon,
+    title: 'Escolha seu Plano',
+    description:
+      'Apresentamos as melhores opções com total transparência. Você decide sem pressão.',
+  },
+  {
+    icon: ThumbsUpIcon,
+    title: 'Equipe Coberta',
+    description:
+      'Cuidamos de toda a burocracia. Sua equipe pode usar o plano em até 24 horas. Suporte em qualquer momento.',
+  },
+]
