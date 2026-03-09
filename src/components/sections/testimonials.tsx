410f5cd4aa74deec5c337d8d627a51cb77326d13
Levy Prado <levyprado28@gmail.com>
2026-03-08 23:18:29 -0400

    feat: add testimonials section

---
+++ src/components/sections/testimonials.tsx
@@ -0,0 +1,64 @@
+import { testimonials } from '@/data/testimonials'
+import { StarIcon } from '@hugeicons/core-free-icons'
+import Container from '../ui/container'
+import Icon from '../ui/icon'
+import Body from '../ui/section/body'
+import Headline from '../ui/section/headline'
+import Section from '../ui/section/section'
+import Tagline from '../ui/section/tagline'
+
+export default function Testimonials() {
+  return (
+    <Section id='depoimentos' className='bg-secondary'>
+      <Container>
+        <div className='space-y-2 text-center lg:space-y-3'>
+          <Tagline>Depoimentos</Tagline>
+          <Headline>Empresários que já economizam</Headline>
+          <Body>
+            Avaliação média de 4.9/5 em +350 avaliações. Mais de 5.200 vidas
+            protegidas e empresas satisfeitas em todo o Brasil.
+          </Body>
+        </div>
+
+        <div className='mt-12 grid gap-6 md:grid-cols-2 lg:mt-16 lg:grid-cols-3'>
+          {testimonials.map((testimonial) => (
+            <div
+              key={testimonial.name}
+              className='row-span-3 grid grid-rows-subgrid gap-6 rounded-2xl bg-background p-6 shadow-sm'
+            >
+              <div className='flex gap-0.5'>
+                {[...Array(testimonial.rating).keys()].map((i) => (
+                  <Icon
+                    key={i}
+                    size={20}
+                    icon={StarIcon}
+                    className='fill-yellow-500 text-yellow-500'
+                  />
+                ))}
+              </div>
+              <p className='text-muted-foreground italic'>
+                &quot;{testimonial.text}&quot;
+              </p>
+              <div className='flex items-center gap-3'>
+                <div className='flex size-12 items-center justify-center rounded-full bg-secondary'>
+                  <span className='text-xl font-bold text-primary'>
+                    {testimonial.name.split(' ')[0][0]}
+                    {testimonial.name.split(' ')[1][0]}
+                  </span>
+                </div>
+                <div className='flex flex-col'>
+                  <div className='truncate text-lg font-semibold'>
+                    {testimonial.name}
+                  </div>
+                  <div className='truncate text-sm text-muted-foreground'>
+                    {testimonial.role}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </Section>
+  )
+}
