0269e3e3250015b1ea32e06961bfbee8af95008c
Levy Prado <levyprado28@gmail.com>
2026-03-15 19:17:01 -0400

    feat: create contact data with phone, email and address

---
+++ src/data/contact.ts
@@ -0,0 +1,19 @@
+import {
+  Call02Icon,
+  Location01Icon,
+  MailIcon,
+} from '@hugeicons/core-free-icons'
+
+export const contact = [
+  { icon: Call02Icon, href: 'tel:+5521992270686', label: '(21) 99227-0686' },
+  {
+    icon: MailIcon,
+    href: 'mailto:adm@snapseguros.com.br',
+    label: 'adm@snapseguros.com.br',
+  },
+  {
+    icon: Location01Icon,
+    label:
+      'Av. das Americas, 7899, bl 2, sl 408 - Barra da Tijuca, Rio de Janeiro, RJ - CEP: 22631-000',
+  },
+]
