7fa26dc39be21f80b216bcb042886becb7191b8f
Levy Prado <levyprado28@gmail.com>
2026-03-17 23:37:31 -0400

    refactor: move TrustBar stats to data file

---
+++ src/data/stats.ts
@@ -0,0 +1,13 @@
+import {
+  ClockIcon,
+  SecurityCheckIcon,
+  StarIcon,
+  UserGroupIcon,
+} from '@hugeicons/core-free-icons'
+
+export const stats = [
+  { icon: UserGroupIcon, value: '5.200+', label: 'Vidas Protegidas' },
+  { icon: StarIcon, value: '4.9/5', label: 'Avaliação Google' },
+  { icon: SecurityCheckIcon, value: '8+', label: 'Operadoras Parceiras' },
+  { icon: ClockIcon, value: '15m', label: 'Tempo de Resposta' },
+]
