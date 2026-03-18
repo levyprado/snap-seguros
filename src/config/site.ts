db301131aca9ed08de56e1feb8f402233c588c37
Levy Prado <levyprado28@gmail.com>
2026-03-17 23:36:02 -0400

    feat: add site config with centralized WHATSAPP_URL and CONTACT constants

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const WHATSAPP_URL =
+  'https://wa.me/5521992270686?text=Ol%C3%A1%2C+gostaria+de+receber+uma+cota%C3%A7%C3%A3o+para+plano+de+sa%C3%BAde+empresarial.'
+
+export const SITE_NAME = 'Snap Seguros'
+
+export const CONTACT = {
+  phone: '(21) 99227-0686',
+  phoneTel: 'tel:+5521992270686',
+  email: 'adm@snapseguros.com.br',
+  emailMailto: 'mailto:adm@snapseguros.com.br',
+  address: 'Barra da Tijuca, Rio de Janeiro - RJ',
+  addressFull:
+    'Av. das Americas, 7899, bl 2, sl 408 - Barra da Tijuca, Rio de Janeiro, RJ - CEP: 22631-000',
+  hours: 'Seg a Sex: 8h às 18h',
+}
