d39457c57ef3e4d112699702e847f5033afe2c33
Levy Prado <levyprado28@gmail.com>
2026-03-17 23:38:42 -0400

    refactor: derive contact data from site config to remove duplication

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,3 +1,4 @@
+import { CONTACT } from '@/config/site'
 import {
   Call02Icon,
   Location01Icon,
@@ -5,15 +6,7 @@ import {
 } from '@hugeicons/core-free-icons'
 
 export const contact = [
-  { icon: Call02Icon, href: 'tel:+5521992270686', label: '(21) 99227-0686' },
-  {
-    icon: MailIcon,
-    href: 'mailto:adm@snapseguros.com.br',
-    label: 'adm@snapseguros.com.br',
-  },
-  {
-    icon: Location01Icon,
-    label:
-      'Av. das Americas, 7899, bl 2, sl 408 - Barra da Tijuca, Rio de Janeiro, RJ - CEP: 22631-000',
-  },
+  { icon: Call02Icon, href: CONTACT.phoneTel, label: CONTACT.phone },
+  { icon: MailIcon, href: CONTACT.emailMailto, label: CONTACT.email },
+  { icon: Location01Icon, label: CONTACT.addressFull },
 ]
